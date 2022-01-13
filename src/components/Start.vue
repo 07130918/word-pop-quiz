e377f33071c3ad1426440a1c1e8cd96f046e16f4
こた <tokyosato1@gmail.com>
2022-01-13 11:04:10 +0900

    startからの遷移(デザインはまだ)

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,19 +1,15 @@
 <template>
     <div>
-        <!-- API通信中はローディング画面入れたい(コンポーネントで) -->
-        <button @click=toQuiz>Start</button>
-        <router-link to="/quiz" class="link" active-class="link--active" exact>
-            Start
-        </router-link>
+        <button @click=moveToQuiz()>Start</button>
     </div>
 </template>
 
 <script>
-export default {
-    methods: {
-        toQuiz() {
-            this.$router.push('quiz');
+    export default {
+        methods: {
+            moveToQuiz() {
+                this.$router.push('quiz');
+            }
         }
     }
-}
 </script>
