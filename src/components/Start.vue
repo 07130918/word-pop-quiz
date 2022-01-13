f955cc54fde7622932848e10deec1c68ac17a06d
こた <tokyosato1@gmail.com>
2022-01-13 11:04:57 +0900

    インデント修正

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -5,11 +5,11 @@
 </template>
 
 <script>
-    export default {
-        methods: {
-            moveToQuiz() {
-                this.$router.push('quiz');
-            }
+export default {
+    methods: {
+        moveToQuiz() {
+            this.$router.push('quiz');
         }
     }
+}
 </script>
