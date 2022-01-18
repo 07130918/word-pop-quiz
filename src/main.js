0436658a521bde705a26adaef7ff3a3ffd28ed65
こた <tokyosato1@gmail.com>
2022-01-19 08:53:56 +0900

    bootstrap vueの導入

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'normalize.css'
 
 import Constants from './const'
