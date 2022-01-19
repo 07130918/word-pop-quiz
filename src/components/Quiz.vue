1611433c45828a152bc6cd48f80e9ba9d1bb01f8
こた <tokyosato1@gmail.com>
2022-01-20 07:52:36 +0900

    順番変え

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -35,15 +35,6 @@
 import mixin from '../mixin'
 
 export default {
-    // router機能: quiz/以下に大きな数字が直接入力された場合Startへ返す
-    beforeRouteEnter(to, from, next) {
-        // vmは勝手に使える
-        next(vm => {
-            if (Number(to.params.num) > vm.words.length) {
-                next('/');
-            }
-        });
-    },
     props: ["words"],
     mixins: [mixin],
     data() {
@@ -52,6 +43,15 @@ export default {
             isAnswered: false,
             questionIndex: this.$route.params.num - 1,
         }
+    },
+    // router機能: quiz/以下に大きな数字が直接入力された場合Startへ返す
+    beforeRouteEnter(to, from, next) {
+        // vmは勝手に使える
+        next(vm => {
+            if (Number(to.params.num) > vm.words.length) {
+                next('/');
+            }
+        });
     },
     created() {
         this.getChoices();
