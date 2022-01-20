0bfeaadeb8812c1f4dd087250cc132909cf2e355
こた <tokyosato1@gmail.com>
2022-01-20 10:04:48 +0900

    デザイン

--- src/components/Goal.vue
+++ src/components/Goal.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="content__wrapper">
-        <div>
-            <h2 class="description">{{ message }}</h2>
-            <button class="btn btn-primary btn-lg" @click=moveToRoot()>Retry</button>
+    <div>
+        <div class="content__wrapper">
+            <div>
+                <h2 class="description">{{ message }}</h2>
+                <button class="btn btn-primary btn-lg" @click=moveToRoot()>Retry</button>
+            </div>
         </div>
     </div>
 </template>
@@ -18,7 +20,7 @@ export default {
     beforeRouteEnter(to, from, next) {
         next(vm => {
             if (Number(from.params.num) === vm.words.length) {
-                vm.message = "You've answered all questions";
+                vm.message = "You've answered all questions!";
             } else {
                 vm.message = "See ya !";
             }
