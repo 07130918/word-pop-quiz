017195d0b5dcdb3f7d0ec8fa3c8f5a0bdc251393
こた <tokyosato1@gmail.com>
2023-11-19 17:12:18 +0900

    Add setQuizzes to QuizzesContext.Provider

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,10 +4,11 @@ import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-    const { quizzes, isLoading, error } = useQuizzes();
+    const { quizzes, setQuizzes, isLoading, error } = useQuizzes();
+
     return (
         <CacheProvider>
-            <QuizzesContext.Provider value={{ quizzes, isLoading, error }}>
+            <QuizzesContext.Provider value={{ quizzes, setQuizzes, isLoading, error }}>
                 <ChakraProvider>
                     {children}
                 </ChakraProvider>
