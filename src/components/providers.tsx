2c3ca1ce2683b6d2502412fb784cfba6220e894f
こた <tokyosato1@gmail.com>
2023-11-19 14:54:04 +0900

    Refactor Providers component to use QuizzesContext

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,17 +1,17 @@
 'use client'
-import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider } from '@chakra-ui/react'
+import { QuizzesContext, useQuizzes } from '@/hooks/quizzes';
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider } from '@chakra-ui/react';
 
-export default function Providers({
-    children
-}: {
-    children: React.ReactNode
-}) {
+export default function Providers({ children }: { children: React.ReactNode }) {
+    const { quizzes, isLoading, error } = useQuizzes();
     return (
         <CacheProvider>
-            <ChakraProvider>
-                {children}
-            </ChakraProvider>
+            <QuizzesContext.Provider value={{ quizzes, isLoading, error }}>
+                <ChakraProvider>
+                    {children}
+                </ChakraProvider>
+            </QuizzesContext.Provider>
         </CacheProvider>
     )
 }
