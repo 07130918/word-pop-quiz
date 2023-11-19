f8f1b52e11e566ce8b9ab2053d366783d1d84657
こた <tokyosato1@gmail.com>
2023-11-19 15:48:17 +0900

    Refactor quiz page layout

--- src/app/quiz/[id]/page.tsx
+++ src/app/quiz/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { QuizzesContext } from '@/hooks/quizzes';
 import type { WordQuizObject } from '@/types/wordObject';
 import { CheckIcon } from '@chakra-ui/icons';
-import { Box, Button, Flex, Heading, Progress } from '@chakra-ui/react';
+import { Box, Button, Heading, Progress, Stack } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
 import { useContext, useEffect, useState } from 'react';
 
@@ -29,71 +29,62 @@ export default function Quiz({ params }: { params: { id: string } }) {
     }, [currentQuizNum, quizzes, router]);
 
     return (
-        <>
-            <Flex w='90vw' m='auto' mt={10}>
-                <Box>
-                    <Box w='90vw'>
-                        <Heading color='whiteAlpha.900'>Question {currentQuizNum}</Heading>
-                        <Progress
-                            value={(currentQuizNum / quizzes.length) * 100}
-                            size='xs'
-                            colorScheme='pink'
-                        />
-                    </Box>
-                    <Box mt={10}>
-                        <Heading color='whiteAlpha.900'>{currentQuiz?.definition}</Heading>
-                        <Flex flexDirection='column' mt={10}>
-                            {currentQuiz?.choices.map((choice) => (
-                                <Box key={choice} mt={4}>
-                                    <Button
-                                        size='lg'
-                                        name={choice}
-                                        colorScheme={
-                                            currentQuiz.english === choice && answered
-                                                ? 'red'
-                                                : 'teal'
-                                        }
-                                        isDisabled={answered}
-                                        onClick={() => {
-                                            setClickedAnswer(choice);
-                                            setAnswered(true);
-                                        }}
-                                    >
-                                        {clickedAnswer === choice &&
-                                            <CheckIcon />
-                                        }
-                                        {choice}
-                                    </Button>
-                                </Box>
-                            ))}
-                        </Flex>
-                        <Box mt={4}>
-                            <Button
-                                size='lg'
-                                marginRight={4}
-                                isDisabled={!answered}
-                                onClick={() => router.push(`/quiz/${currentQuizNum + 1}`)}
-                            >
-                                Move to next
-                            </Button>
-                            {currentQuiz?.url &&
+        <Stack w='90vw' m='auto' mt={10}>
+            <Heading color='whiteAlpha.900'>Question {currentQuizNum}</Heading>
+            <Progress
+                value={(currentQuizNum / quizzes.length) * 100}
+                size='xs'
+                colorScheme='pink'
+            />
+            <Stack mt={10}>
+                <Heading color='whiteAlpha.900'>{currentQuiz?.definition}</Heading>
+                <Stack mt={10}>
+                    {currentQuiz?.choices.map((choice) => {
+                        const isCorrect = currentQuiz.english === choice && answered;
+                        return (
+                            <Box key={choice} mt={4}>
                                 <Button
                                     size='lg'
-                                    colorScheme={
-                                        currentQuiz.english !== clickedAnswer && answered
-                                            ? 'red'
-                                            : 'gray'
-                                    }
-                                    isDisabled={!answered}
-                                    onClick={() => window.open(currentQuiz.url)}
+                                    name={choice}
+                                    colorScheme={isCorrect ? 'red' : 'teal'}
+                                    isDisabled={answered}
+                                    onClick={() => {
+                                        setClickedAnswer(choice);
+                                        setAnswered(true);
+                                    }}
                                 >
-                                    Go to definition
+                                    {clickedAnswer === choice && <CheckIcon />}
+                                    {choice}
                                 </Button>
+                            </Box>
+                        )
+                    })}
+                </Stack>
+                <Box mt={4}>
+                    <Button
+                        size='lg'
+                        mr={4}
+                        isDisabled={!answered}
+                        onClick={() => router.push(`/quiz/${currentQuizNum + 1}`)}
+                    >
+                        Move to next
+                    </Button>
+                    {currentQuiz?.url &&
+                        <Button
+                            size='lg'
+                            colorScheme={
+                                currentQuiz.english !== clickedAnswer && answered
+                                    ? 'red'
+                                    : 'gray'
                             }
-                        </Box>
-                    </Box>
+                            isDisabled={!answered}
+                            onClick={() => window.open(currentQuiz.url)}
+                        >
+                            Go to definition
+                        </Button>
+                    }
                 </Box>
-            </Flex>
-        </>
+            </Stack>
+        </Stack>
     );
 };
