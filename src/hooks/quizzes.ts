d10552b675d4267a922ff593fc04c3ea4ca34adb
こた <tokyosato1@gmail.com>
2023-11-19 16:36:23 +0900

    Update WordQuizObject to WordObject in quizzes.ts

--- src/hooks/quizzes.ts
+++ src/hooks/quizzes.ts
@@ -1,4 +1,4 @@
-import type { WordQuizObject } from '@/types/wordObject';
+import type { WordObject } from '@/types/wordObject';
 import axios from 'axios';
 import { createContext, useEffect, useState } from 'react';
 import type { Dispatch, SetStateAction } from 'react';
@@ -6,20 +6,20 @@ import useSWR from 'swr';
 
 export const QuizzesContext = createContext(
     {} as {
-        quizzes: WordQuizObject[];
-        setQuizzes: Dispatch<SetStateAction<WordQuizObject[]>>;
+        quizzes: WordObject[];
+        setQuizzes: Dispatch<SetStateAction<WordObject[]>>;
         isLoading: boolean;
         error: any;
     },
 );
 
-const fetcher = async (url: string): Promise<WordQuizObject[]> => {
-    const response = await axios.get<WordQuizObject[]>(url);
+const fetcher = async (url: string): Promise<WordObject[]> => {
+    const response = await axios.get<WordObject[]>(url);
     return response.data;
 };
 
 export const useQuizzes = () => {
-    const [quizzes, setQuizzes] = useState<WordQuizObject[]>([]);
+    const [quizzes, setQuizzes] = useState<WordObject[]>([]);
     const {
         data,
         isLoading,
