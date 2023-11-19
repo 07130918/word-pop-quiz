77292c666d01da4228c7f8967336299c4d115994
こた <tokyosato1@gmail.com>
2023-11-19 14:52:03 +0900

    Add quizzes hook

---
+++ src/hooks/quizzes.ts
@@ -0,0 +1,34 @@
+import type { WordQuizObject } from '@/types/wordObject';
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+export const QuizzesContext = createContext(
+    {} as {
+        quizzes: WordQuizObject[];
+        isLoading: boolean;
+        error: any;
+    },
+);
+
+const fetcher = async (url: string): Promise<WordQuizObject[]> => {
+    const response = await axios.get<WordQuizObject[]>(url);
+    return response.data;
+};
+
+export const useQuizzes = () => {
+    const [quizzes, setQuizzes] = useState<WordQuizObject[]>([]);
+    const {
+        data,
+        isLoading,
+        error,
+    } = useSWR('/api/quizzes', fetcher);
+
+    useEffect(() => {
+        if (data) {
+            setQuizzes(data);
+        }
+    }, [data]);
+
+    return { quizzes, isLoading, error };
+}
