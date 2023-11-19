6f7ade3252c5fecac6cb69cc71edda04a1582d10
こた <tokyosato1@gmail.com>
2023-11-19 16:35:52 +0900

    Refactor GET function in quizzes route

--- src/app/api/quizzes/route.ts
+++ src/app/api/quizzes/route.ts
@@ -1,18 +1,14 @@
-import generateQuizResource from '@/libs/generateQuizResource';
-import fisherYatesShuffle from '@/libs/shuffle';
 import type { WordObject } from '@/types/wordObject';
 import axios from 'axios';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
     try {
         const API_URL = process.env.API_URL!;
-        const response = await axios.get<WordObject[]>(API_URL);
-        const quizzes = generateQuizResource(response.data);
-        const shuffledQuizzes = fisherYatesShuffle(quizzes);
-        return NextResponse.json(shuffledQuizzes, { status: 200 });
+        const res = await axios.get<WordObject[]>(API_URL);
+        return NextResponse.json(res.data, { status: 200 });
     } catch (error) {
         console.error(error);
         return NextResponse.json('通信エラーが発生しました。', { status: 500 });
