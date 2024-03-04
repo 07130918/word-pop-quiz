6fd7204e02ecfa1b981b7f324b87fde0c21d1669
こた <tokyosato1@gmail.com>
2024-03-04 18:21:19 +0900

    feat: useToast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@
 import { QuizzesContext } from '@/hooks/quizzes';
 import fisherYatesShuffle from '@/lib/shuffle';
 import {
-    Alert,
-    AlertIcon,
-    AlertTitle,
     Button,
     HStack,
     NumberDecrementStepper,
@@ -14,6 +11,7 @@ import {
     NumberInputStepper,
     Text,
     VStack,
+    useToast,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
 import { useContext, useEffect, useState } from 'react';
@@ -24,10 +22,22 @@ export default function Home() {
         min: 0,
         max: 0,
     });
-
     useEffect(() => {
         setRange({ min: 1, max: quizzes.length });
     }, [quizzes]);
+
+    const toast = useToast();
+    useEffect(() => {
+        if (error) {
+            toast({
+                title: 'Load quizzes failed',
+                description: 'Please try again later',
+                status: 'error',
+                duration: 5000,
+                isClosable: true,
+            });
+        }
+    }, [error, toast]);
 
     const router = useRouter();
     const moveToQuiz = () => {
@@ -35,75 +45,66 @@ export default function Home() {
         setQuizzes(shuffledQuizzes);
         router.push('/quiz/1');
     };
-
     return (
-        <>
-            {error && (
-                <Alert status='error' variant='left-accent'>
-                    <AlertIcon />
-                    <AlertTitle>エラーが発生しました</AlertTitle>
-                </Alert>
-            )}
-            <VStack minH='84vh' justifyContent='center'>
-                <HStack>
-                    <Text color='#FE53BB' fontSize='2xl'>
-                        No.
-                    </Text>
-                    <NumberInput
-                        bg='white'
-                        w='80px'
-                        borderRadius='md'
-                        value={range.min}
-                        min={1}
-                        max={quizzes.length - 5}
-                        onChange={(_, value) =>
-                            setRange((prev) => ({
-                                ...prev,
-                                min: isNaN(value) ? 1 : value,
-                            }))
-                        }
-                    >
-                        <NumberInputField />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
-                    <Text color='#FE53BB' fontSize='2xl'>
-                        &#8211;
-                    </Text>
-                    <NumberInput
-                        bg='white'
-                        w='80px'
-                        borderRadius='md'
-                        value={range.max}
-                        min={5}
-                        max={quizzes.length}
-                        onChange={(_, value) =>
-                            setRange((prev) => ({
-                                ...prev,
-                                max: isNaN(value) ? quizzes.length : value,
-                            }))
-                        }
-                    >
-                        <NumberInputField />
-                        <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
-                </HStack>
-                <Button
-                    size='lg'
-                    color='#FE53BB'
-                    isLoading={isLoading}
-                    isDisabled={error}
-                    loadingText="Let's get started!!!"
-                    onClick={() => moveToQuiz()}
+        <VStack minH='84vh' justifyContent='center'>
+            <HStack>
+                <Text color='#FE53BB' fontSize='2xl'>
+                    No.
+                </Text>
+                <NumberInput
+                    bg='white'
+                    w='80px'
+                    borderRadius='md'
+                    value={range.min}
+                    min={1}
+                    max={quizzes.length - 5}
+                    onChange={(_, value) =>
+                        setRange((prev) => ({
+                            ...prev,
+                            min: isNaN(value) ? 1 : value,
+                        }))
+                    }
                 >
-                    Let&apos;s get started !!!
-                </Button>
-            </VStack>
-        </>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+                <Text color='#FE53BB' fontSize='2xl'>
+                    &#8211;
+                </Text>
+                <NumberInput
+                    bg='white'
+                    w='80px'
+                    borderRadius='md'
+                    value={range.max}
+                    min={5}
+                    max={quizzes.length}
+                    onChange={(_, value) =>
+                        setRange((prev) => ({
+                            ...prev,
+                            max: isNaN(value) ? quizzes.length : value,
+                        }))
+                    }
+                >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+            </HStack>
+            <Button
+                size='lg'
+                color='#FE53BB'
+                isLoading={isLoading}
+                isDisabled={error}
+                loadingText="Let's get started!!!"
+                onClick={() => moveToQuiz()}
+            >
+                Let&apos;s get started !!!
+            </Button>
+        </VStack>
     );
 }
